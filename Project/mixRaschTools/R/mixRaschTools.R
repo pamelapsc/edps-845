3d9b70db59b8dcbe1aae27d3f1b5b39ca392f3af
Pam Trantham <pamelapsc@yahoo.com>
2016-06-03 20:19:43 -0500

    Saving Changes

--- Project/mixRaschTools/R/mixRaschTools.R
+++ Project/mixRaschTools/R/mixRaschTools.R
@@ -1,7 +1,6 @@
 
 #' mixRaschTools Functions -----------------------------------------------------------
 #' @name Item parameter plot for mixed Rasch model
-#' @alias Item parameter plot for mixed Rasch model
 #' @title Function for producing plots of item parameters for multiple class models.
 #' @description This function produces item parameters by class for Rasch calibrated mixture models.
 #' @usage
@@ -15,6 +14,7 @@
 #' data(threeclass_ex)
 #' Default item paramater plot
 #' mixRasch.plot(threeclass_ex)
+#' @export
 mixRasch.plot <- function(x, ...){
   nc<- length(x$LatentClass)
     plottemp <- sapply(x$LatentClass, function(x) x$item.par$delta.i)
@@ -26,7 +26,6 @@ mixRasch.plot <- function(x, ...){
 
 #' mixRasch Average Theta -----------------------------------------------------------
 #' @name Average Ability Levels for Multiple Class Mixed Rasch Model
-#' @alias Average Ability Levels for Multiple Class Mixed Rasch Model
 #' @title Function for producing mean theta values for each class in a mixture Rasch model.
 #' @description This function produces mean ability levels for each class in a mixture Rasch model.
 #' @usage
@@ -40,11 +39,13 @@ mixRasch.plot <- function(x, ...){
 #' data(threeclass_ex)
 #' Default item paramater plot
 #' avg.theta(threeclass_ex)
+#' @export
 avg.theta<- function(x){
   nc<- length(x$LatentClass)
     outtheta <- sapply(x$LatentClass, function(x) mean(x$person.par$theta))
   return(outtheta)
 }
+
 
 
 
